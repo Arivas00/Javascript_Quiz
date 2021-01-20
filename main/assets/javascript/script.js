ff72a89b82d633d85ddf30c9b89ab2b6c79633ef
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-19 21:12:52 -0500

    Linked bootstrap, javascript and added buttons.

---
+++ main/assets/javascript/script.js
@@ -0,0 +1,17 @@
+//index page
+var highscoreBtn = document.querySelector("#scores");
+
+highscoreBtn.addEventListener("click", function() {
+    window.location.href = "./main/high_scores.html"
+})
+
+
+//highscore page
+
+var restartBtn = document.querySelector("#restart")
+var clearBtn = document.querySelector("#clear")
+
+
+restartBtn.addEventListener("click", function() {
+    console.log("checking button")
+})
