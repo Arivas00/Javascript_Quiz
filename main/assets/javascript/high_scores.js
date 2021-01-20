2d0729962f731260a9d3d85f273405f946485ca7
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-20 08:59:35 -0500

    Fixed restart button and created two javascript files

---
+++ main/assets/javascript/high_scores.js
@@ -0,0 +1,9 @@
+var restartBtn = document.querySelector("#restart")
+var clearBtn = document.querySelector("#clear")
+
+
+restartBtn.addEventListener("click", function() {
+    
+        window.location.href = "../index.html"
+
+})
---
+++ main/assets/javascript/index.js
@@ -0,0 +1,10 @@
+//inde
+var highscoreBtn = document.querySelector("#scores");
+
+highscoreBtn.addEventListener("click", function() {
+    
+        window.location.href = "./main/high_scores.html";
+
+})
+
+
