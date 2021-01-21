9016a562ba1382b147bf2ae1dbc53daf01cdc94a
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-20 22:17:38 -0500

    Added questions and answers

--- main/assets/javascript/index.js
+++ main/assets/javascript/index.js
@@ -11,6 +11,50 @@ var startBtn = document.querySelector("#startBtn");
 var title = document.querySelector("#title");
 var info = document.querySelector("#info");
 
+var questionsAnswers = [
+
+    {question: 'How do you write "Hello World" in an alert box?',
+    choice1: 'alertBox("Hello World");',
+    choice2: 'msgBox("Hello World");',
+    choice3: 'msg("Hello World");',
+    choice4: 'alert("Hello World");',
+    correct: "choice4",
+    },
+
+    
+    {question: "How do you create a function in JavaScript?",
+    choice1: "function myFunction()",
+    choice2: "function = myFunction()",
+    choice3: "function : myFunction()",
+    choice4: "function * myFunction()",
+    correct: "choice2",
+    },  
+
+    {question: 'How do you write an IF statement in JavaScript?',
+    choice1: "if i === 5 then",
+    choice2: "if i = 5",
+    choice3: "if (i === 5)",
+    choice4: "if i = 5 then",
+    correct: "choice3",
+    },
+
+    {question: "How does a FOR loop start?",
+    choice1: "for (var i = 0; i < 5; i++)",
+    choice2: "for (var i = 0; i < 5)",
+    choice3: "for (i < 5; i++)",
+    choice4: "for i = 1 to 5",
+    correct: "choice1",
+    },
+
+    {question: "How do you single line comment in Javascript?",
+    choice1: "<!-- This is a comment -->",
+    choice2: "//This is a comment",
+    choice3: "*This is a comment*",
+    choice4: "{This is a comment}",
+    correct: "choice2",
+    }
+];
+
 function startQuiz() {
 
     title.remove();
